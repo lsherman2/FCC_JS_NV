a55bf8d536834402448e96f339548f243fb4eae6
lsherman2 <logansherman1@gmail.com>
2024-02-02 22:15:49 -0600

    Added function to check for the correct country code.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const check = document.getElementById('check-btn');
 const clear = document.getElementById('clear-btn');
 const result = document.getElementById('results-div');
 
-const first = /(?:^|\s)1 [0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
+const first = /(?:^|\s)([1-9])+ [0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
 const second = /(?:^|\s)1 \([0-9][0-9][0-9]\) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
 const third = /(?:^|\s)1\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
 const fourth = /(?:^|\s)1 [0-9][0-9][0-9] [0-9][0-9][0-9] [0-9][0-9][0-9][0-9](?:$|\s)/;
@@ -13,13 +13,33 @@ const seventh = /(?:^|\s)\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]
 
 const acceptable = [first, second, third, fourth, fifth, sixth, seventh];
 
-const isCorrect = (num) => acceptable.some((regex) => regex.test(num));
+const checkCountry = (num) => {
+  const regex = /(?:^)[0-9][0-9]?[\s\(]/;
+  const country = num.slice().match(regex);
+  if (country !== null) {
+    const countryNum = parseInt(country[0]);
+    if (countryNum !== 1) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const isCorrect = (num) => {
+  if (checkCountry(num)) {
+    return acceptable.some((regex) => regex.test(num))
+  } else {
+    return false
+  }
+};
 
 const validate = () => {
   if (input.value === '') {
     alert("Please provide a phone number");
     return
   }
+
+  checkCountry(input.value);
 
   result.textContent = isCorrect(input.value)
     ? `Valid US number: ${input.value}`
