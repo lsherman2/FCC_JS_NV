cbb3ef940be10698da7ea6ff7b682857f02e4fa1
lsherman2 <logansherman1@gmail.com>
2024-02-02 21:28:19 -0600

    Added functionality for checking and made the regex more complex.

--- script.js
+++ script.js
@@ -3,22 +3,28 @@ const check = document.getElementById('check-btn');
 const clear = document.getElementById('clear-btn');
 const result = document.getElementById('results-div');
 
-const first = /1 [0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]/;
-const second = /1 \([0-9][0-9][0-9]\) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]/;
-const third = /1\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]/;
-const fourth = /1 [0-9][0-9][0-9] [0-9][0-9][0-9] [0-9][0-9][0-9][0-9]/;
-const fifth = /[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/;
-const sixth = /[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]/;
-const seventh = /\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]/;
+const first = /(?:^|\s)1 [0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
+const second = /(?:^|\s)1 \([0-9][0-9][0-9]\) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
+const third = /(?:^|\s)1\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
+const fourth = /(?:^|\s)1 [0-9][0-9][0-9] [0-9][0-9][0-9] [0-9][0-9][0-9][0-9](?:$|\s)/;
+const fifth = /(?:^|\s)[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9](?:$|\s)/;
+const sixth = /(?:^|\s)[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
+const seventh = /(?:^|\s)\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9](?:$|\s)/;
 
 const acceptable = [first, second, third, fourth, fifth, sixth, seventh];
+
+const isCorrect = (num) => acceptable.some((regex) => regex.test(num));
 
 const validate = () => {
   if (input.value === '') {
     alert("Please provide a phone number");
     return
   }
-  
+
+  result.textContent = isCorrect(input.value)
+    ? `Valid US number: ${input.value}`
+    : `Invalid US number: ${input.value}`;
+  input.value = "";
 };
 
 check.addEventListener("click", validate);
